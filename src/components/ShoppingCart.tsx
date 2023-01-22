b36899e7a4eeb887241d8e0f30d9a2a0e3dbe0a6
victor.mbarachi@halogen-group.com <39647480+Mbarachi@users.noreply.github.com>
2023-01-22 23:43:03 +0100

    Added the CartItem component and did some extra work on the ShoppingCart Component

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,16 +1,35 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/FormatCurrency";
+import CartItem from "./CartItem";
+import storeItems from "../data/items.json";
 
 type ShoppingTypeProps = {
   isOpen: boolean;
 };
 const ShoppingCart = ({ isOpen }: ShoppingTypeProps) => {
-  const { closeCart, openCart } = useShoppingCart();
+  const { closeCart, openCart, cartItems } = useShoppingCart();
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement="end">
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+        </Stack>
+        <div className="fw-bold fs-5" style={{float:"right"}}>
+          Total:{" "}
+          {formatCurrency(
+            cartItems.reduce((total, cartItem) => {
+              const item = storeItems.find((item) => item.id === cartItem.id);
+              return total + (item?.price || 0) * cartItem.quantity;
+            }, 0)
+          )}
+        </div>
+      </Offcanvas.Body>
     </Offcanvas>
   );
 };
